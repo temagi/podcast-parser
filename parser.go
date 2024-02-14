57dcfefca13b76a41e0603f1cebb5d4112e7d3ce
Artem Gizzatullin <tema.gizz@gmail.com>
2024-02-14 19:48:02 +0100

    Add bootstap and small improvements

--- parser.go
+++ parser.go
@@ -24,51 +24,56 @@ func urlExists(url string) bool {
 	return false
 }
 
-// Function to generate the HTML page with links to mp3 files
+// Function to generate the HTML page with links to mp3 files in table format with Bootstrap styling
 func generateHTMLPage() string {
 	const numLinks = 15
 
 	// Get the previous day's date
 	prevDate := time.Now().AddDate(0, 0, -1)
 
-	// Base URL with dynamic part
-	baseURL := "https://itunes.radiorecord.ru/tmp_audio/"
-
 	var htmlBuilder strings.Builder
 
-	// HTML page header
-	htmlBuilder.WriteString("<!DOCTYPE html><html><head><title>MP3 Files</title></head><body><h1>MP3 Files</h1><ul>")
+	// HTML page header with Bootstrap CSS included
+	htmlBuilder.WriteString("<!DOCTYPE html><html><head><title>Latest RadioRecord K&H podcast direct links</title>")
+	htmlBuilder.WriteString(`<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">`)
+	htmlBuilder.WriteString("</head><body><div class=\"container\"><h1 class=\"mt-5 mb-4\">Latest RadioRecord K&H podcast direct links</h1><table class=\"table\"><thead><tr><th>#</th><th>Date</th><th>Day of the Week</th><th>Link</th><th>Status</th></tr></thead><tbody>")
 
-	// Loop through previous dates and generate links
+	// Loop through previous dates and generate table rows
 	for i := 0; i < numLinks; i++ {
 		// Construct the URL for the mp3 file
 		dateStr := prevDate.Format("2006-01-02")
-		url := fmt.Sprintf("%situnes1/hik_-_rr_%s.mp3", baseURL, dateStr)
+		url := fmt.Sprintf("https://itunes.radiorecord.ru/tmp_audio/itunes1/hik_-_rr_%s.mp3", dateStr)
 
-		htmlBuilder.WriteString("<li>")
-		htmlBuilder.WriteString(fmt.Sprintf("<a href=\"%s\">%s</a>", url, url))
+		htmlBuilder.WriteString("<tr>")
+		htmlBuilder.WriteString(fmt.Sprintf("<td>%d</td>", i+1))
+		htmlBuilder.WriteString(fmt.Sprintf("<td>%s</td>", prevDate.Format("2006-01-02")))
+		htmlBuilder.WriteString(fmt.Sprintf("<td>%s</td>", prevDate.Weekday().String()))
+		htmlBuilder.WriteString(fmt.Sprintf("<td><a href=\"%s\">Download file</a></td>", url))
 
 		// Check if the mp3 file is available
+		status := "Not Available"
 		if urlExists(url) {
-			htmlBuilder.WriteString(" (Available)")
+			status = "Available"
+			htmlBuilder.WriteString(fmt.Sprintf("<td style=\"background-color: lightgreen;\">%s</td>", status))
 		} else {
 			// Try the second dynamic part if the first one is not available
 			url2 := strings.Replace(url, "itunes1", "itunes2", 1)
 			if urlExists(url2) {
-				htmlBuilder.WriteString(" (Available in itunes2)")
+				status = "Available"
+				htmlBuilder.WriteString(fmt.Sprintf("<td style=\"background-color: lightgreen;\">%s</td>", status))
 			} else {
-				htmlBuilder.WriteString(" (Not Available)")
+				htmlBuilder.WriteString(fmt.Sprintf("<td style=\"background-color: lightcoral;\">%s</td>", status))
 			}
 		}
 
-		htmlBuilder.WriteString("</li>")
+		htmlBuilder.WriteString("</tr>")
 
 		// Decrement the date for the next iteration
 		prevDate = prevDate.AddDate(0, 0, -1)
 	}
 
 	// HTML page footer
-	htmlBuilder.WriteString("</ul></body></html>")
+	htmlBuilder.WriteString("</tbody></table></div></body></html>")
 
 	return htmlBuilder.String()
 }
